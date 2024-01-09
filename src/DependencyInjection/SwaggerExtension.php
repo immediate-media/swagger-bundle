d2b1c945e3309dd016743fd89ebfbe858c198892
mmarcinkowski <marcin.marcinkowski@burdamedia.pl>
2024-01-09 15:53:35 +0100

    decorator services

---
+++ src/DependencyInjection/SwaggerExtension.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace IM\Fabric\Bundle\SwaggerBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class SwaggerExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $configuration = new Configuration();
+        $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader->load('services.yaml');
+    }
+}
