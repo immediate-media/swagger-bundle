3e7aca59990b187a413d24d2b9e3c82ec345206d
alex.ferreira <alex.ferreira@immediate.co.uk>
2026-01-13 12:10:43 +0000

    feat: updated use to please Symfony 7.4 depracation error

--- src/DependencyInjection/SwaggerExtension.php
+++ src/DependencyInjection/SwaggerExtension.php
@@ -7,7 +7,7 @@ namespace IM\Fabric\Bundle\SwaggerBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class SwaggerExtension extends Extension
 {
